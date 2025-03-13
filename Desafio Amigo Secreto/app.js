3aa3157640c8a7eab8b567a395d63a112e09c483
IgnacioRubiolo26 <nachorubiolo26@gmail.com>
2025-03-12 23:31:27 -0300

    Add files via upload

---
+++ Desafio Amigo Secreto/app.js
@@ -0,0 +1,59 @@
+
+let amigos = [];
+let listaDeAmigos = document.getElementById('listaAmigos');
+let amigoSorteado = document.getElementById('resultado');
+let reiniciar = document.getElementById('reiniciar');
+
+
+function agregarAmigo() {
+    
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    if (amigos.includes(nombreAmigo)) {
+        alert('No repetir nombre, usar segundo nombre de ser necesario.');
+        amigos.pop(nombreAmigo);
+    }
+
+    if (nombreAmigo === '') {
+        alert('El campo está vacío, debe escribir un nombre.');
+    } else {
+        amigos.push(nombreAmigo);
+    }       
+    actualizarListaDeAmigos();
+    limpiarCaja();
+    return;    
+}  
+
+function actualizarListaDeAmigos() {
+    listaDeAmigos.innerHTML = '';
+    for (var i = 1; i <= amigos.length; i++) {
+        let nombreAgregado = document.createElement('li');
+        nombreAgregado.textContent = ` ${amigos[i - 1]}`;
+        listaDeAmigos.appendChild(nombreAgregado);
+    }
+}
+
+function sortearAmigo() {
+
+    if (amigos.length >= 2) {
+        let amigoAleatorio = Math.floor(Math.random() * amigos.length);
+        amigoSorteado.innerHTML = `El amigo sorteado es: ${amigos[amigoAleatorio]}.`;
+    } else {
+        alert('Debe añadir al menos dos nombres.');
+    } 
+
+}
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
+function nuevaLista() {
+    if (amigos.length !='') {
+    location.reload();
+    amigos.splice(0,amigos.length);
+    } else {
+        alert('La lista se encuentra vacia.');
+    }
+  
+}
+
+
